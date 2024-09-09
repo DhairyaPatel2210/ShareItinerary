921f7c8acac51af59eda7349098f6230fcde25f9
Dhairya Patel <dhairyapatel@Dhairyas-Air.lan>
2024-09-09 16:29:33 -0500

    adding roles for kubernetes

--- src/main/java/com/shareitinerary/ShareitineraryApplication.java
+++ src/main/java/com/shareitinerary/ShareitineraryApplication.java
@@ -2,8 +2,10 @@ package com.shareitinerary;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ShareitineraryApplication {
 
 	public static void main(String[] args) {
